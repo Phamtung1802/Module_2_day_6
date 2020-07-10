9f1aeef2d7a84ac44820ed3c103fed5661299488
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-10 10:35:57 +0700

    add BaiTapTrienKhaiLopTriangle

---
+++ BaiTapTrienKhaiLopTriangle/src/baitaptrienkhailoptriangle/TestTriangle.java
@@ -0,0 +1,9 @@
+package baitaptrienkhailoptriangle;
+
+public class TestTriangle {
+    public static void main(String[] args) {
+        Triangle Triangle;
+        Triangle = new Triangle("Red", true, 3.0,4.0,5.0);
+        System.out.println(Triangle.toString());
+    }
+}
